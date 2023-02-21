23050b9cec67eb1242845fd84ce9ac9c9be468ff
Ovuzo <ovuzonyobeno@gmail.com>
2023-02-21 01:40:20 +0100

    updated task

---
+++ index.js
@@ -0,0 +1,25 @@
+
+// let username = "Bola"
+// let group1 = "Physics, Chemistry, Biology, Technical Drawing";
+// let group2 = "Accounting, Commerce, Marketing, Geography";
+// let group3 = "Government, Economics, Literature, History";
+// let group4 = "English, Mathematics";
+// let input = prompt('enter your class group')
+
+//  if(input === "science"){
+//     prompt(`Hi ${username} the list of your offered subjects are ${group1} including ${group4}`)
+//  } else if(input ==="socialScience"){
+//     prompt(`Hi ${username} the list of your offered subjects are ${group2} including ${group4}`) 
+//  } else if(input === "arts"){
+//     prompt(`Hi ${username} the list of your offered subjects are ${group3} including ${group4}`) 
+//  } else prompt(`${username} you are offerring only ${group4}`)
+
+
+
+
+// let lastNum;
+// let num =50;
+// for(let i =2; i < num; i =i ** 2){
+//     lastNum = i;
+// }
+// console.log(`The number ${lastNum} is the power of 2 nearest to ${num}`)
---
+++ operators-control-flow/readme.md
@@ -0,0 +1,11 @@
+List the symbols for each of the below Javascript operators
+Arithmetic Operators -   + - * / %
+Assignment Operators -   =  +=  -=  *=  /=  %=
+Comparison Operators -   ==  ===  !=  !==  <   >  =<   =>
+Logical Operators    -   &&   ||   !
+Bitwise Operators
+
+
+4  The initial value of i will be 1, when it runs again i gets 8, it checks the condition and run again it becomes 15, it checks the condition and discovered it has met it and it stop running.
+
+
